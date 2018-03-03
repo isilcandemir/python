5072758b4639f4da04004a432b53bd4ccf55d9f2
Isil Candemir <32885773+isilcandemir@users.noreply.github.com>
2018-03-03 17:04:39 +0300

    Add files via upload

---
+++ Python_Basics.py
@@ -0,0 +1,31 @@
+
+#Basic math operators are the same as those in R, except power, **
+
+#assignment operator is =
+
+#structures are: float, integer, str, bool. type(x)
+
+#change the type of values int(), or str()
+
+#zero-based indexing, also negative indexing x[-1] tail of the list, [sart:end], end exclusive
+
+#list of lists: x = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]], x[2][0]
+
+#adding element: use plus sign, for remove del(x[1])
+
+#don't use y=x, but use y=list(x) or y=x[:]
+
+# ; two commands on the same line
+
+#calling functions: room.upper()
+
+#areas.append(2); print(areas.reverse())
+
+#import numpy as np, or import math, then call functions np.array or math.pi
+#if you call a specific function like from math import pi, then use pi alone, not math.pi
+
+#numpy, if you have python list isil=[1,2,3] to change it array numpy.array(isil)
+#to get dimension of array isil.shape
+#isil=np.array([[1,2,3],[4,5,6]])
+
+
