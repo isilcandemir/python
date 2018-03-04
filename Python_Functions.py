27eee0840f1c4d1634a4d17c293c6e2a3cc6455f
Isil Candemir <32885773+isilcandemir@users.noreply.github.com>
2018-03-04 17:06:39 +0300

    Update Python_Functions.py

--- Python_Functions.py
+++ Python_Functions.py
@@ -58,3 +58,18 @@ for x in range(10):
     else:
         outcomes.append("tails")
 print(outcomes)   
+
+import numpy as np
+np.random.seed(123)
+final_tails = []
+for x in range(100000):
+    tails=[0]
+    for x in range(10):
+        coin = np.random.randint(0,2)
+        tails.append(tails[x] + coin)
+    final_tails.append(tails[-1])
+print(final_tails)
+
+import matplotlib.pyplot as plt
+plt.hist(final_tails, bins = 10)   
+plt.show()
