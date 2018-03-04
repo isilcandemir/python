f7a3733c5e157654b9bd1dd9a6e37f21014859dd
Isil Candemir <32885773+isilcandemir@users.noreply.github.com>
2018-03-04 23:23:13 +0300

    Add files via upload

---
+++ Writing_Functions.py
@@ -0,0 +1,18 @@
+def square(value):
+    new_value = value**2
+    return new_value
+    
+square(4)
+
+y=square(4)
+print(y)
+
+def mod2plus5(x1, x2, x3):
+    def inner(x):
+        return x%2+5
+    return(inner(x1), inner(x2), inner(x3))
+    
+raise_to_power=lambda x,y: x**y
+
+map(lambda x: x**2, seq)
+
