4c3c2b38dcef7f540bc309141179aec0ce460984
Isil Candemir <32885773+isilcandemir@users.noreply.github.com>
2018-03-05 17:19:34 +0300

    Update Reading_Files.py

--- Reading_Files.py
+++ Reading_Files.py
@@ -18,5 +18,16 @@ import pandas as pd
 file="winequality-red.csv"
 data=pd.read_csv(filename)
 
+#for excel
+import pandas as pd
+file="urbanpop.xlsx"
+data=pd.ExcelFile(file)
+print(data.sheet_names)
+df1 = xl.parse('2004')
+print(df1.head())
 
+# Parse the first sheet and rename the columns: df1
+df1 = xl.parse(0, skiprows=[0], names=['Country', 'AAM due to War (2002)'])
 
+# Parse the first column of the second sheet and rename the column: df2
+df2 = xl.parse(1, parse_cols=[0], skiprows=[0], names=['Country'])
