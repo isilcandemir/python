3c0854dd18bc3a1c31c2b565136bbe915429b18d
Isil Candemir <32885773+isilcandemir@users.noreply.github.com>
2018-03-05 16:55:35 +0300

    Update Reading_Files.py

--- Reading_Files.py
+++ Reading_Files.py
@@ -6,3 +6,17 @@ print(file.read())
 print(file.closed)
 file.close()
 print(file.closed)
+
+#for data containing only number
+import numpy as np
+file = 'digits.csv'
+digits = np.loadtxt(file, delimiter=",")
+print(type(digits))
+
+# for everytype data
+import pandas as pd
+file="winequality-red.csv"
+data=pd.read_csv(filename)
+
+
+
